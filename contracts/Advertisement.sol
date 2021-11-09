ce942b6ac4ea3628e85976e3dc9b1e4b8a9929cd
damiansarnecki <xdamiansarnecki@gmail.com>
2021-11-09 03:38:24 +0100

    started testing

--- contracts/Advertisement.sol
+++ contracts/Advertisement.sol
@@ -19,12 +19,24 @@ contract Advertisement is Ownable {
     mapping(uint => address) public adToOwner;
     mapping(address => uint) public ownerAdCount;
 
-    function createAd(uint16 _day, uint16 _spot, string memory _text) internal onlyOwner {
+    function createAd(uint16 _day, uint16 _spot, string memory _text) public onlyOwner {
         ads.push(Ad(_day, _spot, _text));
         uint id = ads.length - 1;
         adToOwner[id] = msg.sender;
         ownerAdCount[msg.sender]++;
+    }
 
+    function getAds() public view returns(Ad[] memory) {
+        return ads;
+    }
+
+    modifier isAdOwner(uint _index) {
+        require(msg.sender == adToOwner[_index], "You are not ad owner");
+        _;
+    }
+
+    function editAdText(uint _index, string memory _text) public isAdOwner(_index) {
+        ads[_index].text = _text;
     }
 
     uint public x = 5;
--- test/index.ts
+++ test/index.ts
@@ -13,6 +13,7 @@ describe("Advertisement", () => {
   beforeEach(async () => {
     Advertisement = await ethers.getContractFactory("Advertisement");
     advertisement = await Advertisement.deploy()
+    await advertisement.deployed();
 
     let signers = await ethers.getSigners();
     owner = signers[0];
@@ -24,6 +25,29 @@ describe("Advertisement", () => {
   describe('Deployment', () => {
     it('Sets correct owner', async () => {
       expect(await advertisement.owner()).to.equal(owner.address);
+    })
+  })
+
+  describe('Adding ads', () => {
+
+    it('Adds new advertisement', async () => {
+      await advertisement.createAd(1,2,"test");
+      expect((await advertisement.getAds()).length).to.equal(1);
+    })
+
+    it('Sets values correctly', async () => {
+      await advertisement.createAd(2,3,"test text");
+      expect((await advertisement.getAds())[0].day).to.equal(2); //day
+      expect((await advertisement.getAds())[0].spot).to.equal(3); //spot
+      expect((await advertisement.getAds())[0].text).to.equal("test text"); //text
+    })
+  })
+
+  describe('Editing ads', () => {
+    it('prevents editing if not owner', async () => {
+      await advertisement.createAd(2,3,"test text");
+      await advertisement.editAdText(0, "essa");
+      expect((await advertisement.getAds())[0].text).to.equal("essa");
     })
   })
 
